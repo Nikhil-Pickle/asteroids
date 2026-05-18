21e4d0933dab9114d03e4a3999a5745e3bc2158c
Nikhil B <94644754+Nikhil-Pickle@users.noreply.github.com>
2026-05-18 15:51:07 -0400

    asteroids split on kill

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH, PLAYER_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +15,17 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        first_new_velocity = self.velocity.rotate(random_angle)
+        second_new_velocity = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        first_new_asteroid.velocity = first_new_velocity * 1.2
+        second_new_asteroid.velocity = second_new_velocity * 1.2
